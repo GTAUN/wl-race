aa54dda01384bf4655bf19b77fba7d143e09a185
mk124 <mk124a@gmail.com>
2014-05-30 23:13:11 +0800

    Bug fixes

--- src/main/java/net/gtaun/wl/race/dialog/CodeEditorDialog.java
+++ src/main/java/net/gtaun/wl/race/dialog/CodeEditorDialog.java
@@ -35,47 +35,47 @@ import org.apache.commons.lang3.StringUtils;
 
 /**
  * JavaScript 代码编辑器。
- * 
+ *
  * @author MK124
  */
 public class CodeEditorDialog extends WlPageListDialog
 {
 	private final PlayerStringSet stringSet;
-	
+
 	private List<String> codeLines;
 	private Consumer<String> onCompleteHandler;
-	
-	
+
+
 	public CodeEditorDialog(Player player, EventManager eventManager, AbstractDialog parent, RaceServiceImpl service, String title, String code, Consumer<String> onCompleteHandler)
 	{		super(player, eventManager);
 		setParentDialog(parent);
-		
+
 		this.onCompleteHandler = onCompleteHandler;
-		
+
 		if (code == null) code = "";
 		codeLines = new ArrayList<>(Arrays.asList(StringUtils.split(code, '\n')));
 
 		stringSet = service.getLocalizedStringSet().getStringSet(player);
 		setCaption(() -> stringSet.format("Dialog.CodeEditorDialog.Caption", title, codeLines.size()));
 	}
-	
+
 	@Override
 	public void show()
 	{
 		items.clear();
-		
+
 		for (int i=0; i<codeLines.size(); i++)
 		{
 			String line = codeLines.get(i);
 			int lineNum = i+1;
-			
+
 			addItem(stringSet.format("Dialog.CodeEditorDialog.Item", lineNum, line), (item) ->
 			{
 				player.playSound(1083);
-				
+
 				String caption = stringSet.format("Dialog.CodeEditorEditLineDialog.Caption", lineNum);
 				String message = stringSet.format("Dialog.CodeEditorEditLineDialog.Text", lineNum, line);
-				
+
 				WlInputDialog.create(player, rootEventManager)
 					.parentDialog(this)
 					.caption(caption)
@@ -83,7 +83,7 @@ public class CodeEditorDialog extends WlPageListDialog
 					.onClickOk((d, text) ->
 					{
 						player.playSound(1083);
-						
+
 						if (text.equalsIgnoreCase("INSERT"))
 						{
 							showInsertLineDialog(lineNum, d.getParentDialog());
@@ -92,39 +92,39 @@ public class CodeEditorDialog extends WlPageListDialog
 						else if (text.equalsIgnoreCase("DELETE"))
 						{
 							codeLines.remove(lineNum-1);
-							showParentDialog();
+							d.showParentDialog();
 							return;
 						}
-						
+
 						codeLines.set(lineNum-1, text);
-						showParentDialog();
+						d.showParentDialog();
 					})
 					.build().show();
 			});
 		}
-		
+
 		addItem(stringSet.get("Dialog.CodeEditorDialog.Add"), (i) ->
 		{
 			player.playSound(1083);
-			
+
 			int lineNum = codeLines.size() + 1;
 			showInsertLineDialog(lineNum, CodeEditorDialog.this);
 		});
-		
+
 		addItem(stringSet.get("Dialog.CodeEditorDialog.Save"), (i) ->
 		{
 			player.playSound(1083);
 			onCompleteHandler.accept(toCode());
 		});
-		
+
 		super.show();
 	}
-	
+
 	private void showInsertLineDialog(int line, AbstractDialog dialog)
 	{
 		String caption = stringSet.format("Dialog.CodeEditorNewLineDialog.Caption", line);
 		String message = stringSet.format("Dialog.CodeEditorNewLineDialog.Text", line);
-		
+
 		WlInputDialog.create(player, rootEventManager)
 			.parentDialog(dialog)
 			.caption(caption)
@@ -132,13 +132,13 @@ public class CodeEditorDialog extends WlPageListDialog
 			.onClickOk((d, text) ->
 			{
 				player.playSound(1083);
-				
+
 				codeLines.add(line-1, text);
 				d.showParentDialog();
 			})
 			.build().show();
 	}
-	
+
 	public String toCode()
 	{
 		StringBuilder builder = new StringBuilder();
@@ -147,7 +147,7 @@ public class CodeEditorDialog extends WlPageListDialog
 			builder.append(line);
 			builder.append("\n");
 		}
-		
+
 		return builder.toString();
 	}
 }
